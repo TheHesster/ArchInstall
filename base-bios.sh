0f426352ae44ccf978c42c47cded60b0bdd67315
Adam <85858098+TheHesster@users.noreply.github.com>
2022-01-08 10:14:20 -0800

    Create base-bios.sh

---
+++ base-bios.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/America/Vancouver /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+pacman -S grub vim networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp sof-firmware nss-mdns acpid ntfs-3g
+
+# pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+grub-install --target=i386-pc /dev/sdX
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable acpid
+
+useradd -m adam
+echo adam:password-here | chpasswd
+
+echo "adam ALL=(ALL) ALL" >> /etc/sudoers.d/adam
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
