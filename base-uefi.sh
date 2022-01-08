0588410414391f402e415a047b3d9963744b3d60
Adam <85858098+TheHesster@users.noreply.github.com>
2022-01-08 09:47:21 -0800

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -14,7 +14,7 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr vim networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp sof-firmware nss-mdns acpid ntfs-3g
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
@@ -29,11 +29,10 @@ systemctl enable avahi-daemon
 systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
-systemctl enable firewalld
 systemctl enable acpid
 
 useradd -m adam
-echo ermanno:password | chpasswd
+echo adam:password-here | chpasswd
 
 echo "adam ALL=(ALL) ALL" >> /etc/sudoers.d/adam
 
